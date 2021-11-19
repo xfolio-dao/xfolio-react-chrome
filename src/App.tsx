dde643c640fd6282cde773d0fa83f90e6b30ed79
exakoss <danielkossman@yahoo.com>
2021-11-18 23:18:02 -0500

    added context, types and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,27 @@ import { Route, Routes, MemoryRouter as Router } from 'react-router-dom'
 import Wallet from './pages/wallet'
 import Login from './pages/login'
 import Settings from './pages/settings'
+import { WalletContext } from './context/walletContext'
+import { useDefineWalletContext } from './hooks'
 
 const App = () => {
+    const walletContextValue = useDefineWalletContext()
+
     return (
-        <Router
-            initialEntries={['/login','/wallet']}
-            initialIndex={0}
-        >
-            <Fragment>
-                <Routes>
-                    <Route path='/login' element={ <Login/> }/>
-                    <Route path='/wallet' element={ <Wallet/> }/>
-                    <Route path='/settings' element={ <Settings/> }/>
-                </Routes>
-            </Fragment>
-        </Router>
+        <WalletContext.Provider value={walletContextValue}>
+            <Router
+                initialEntries={['/login','/wallet']}
+                initialIndex={0}
+            >
+                <Fragment>
+                    <Routes>
+                        <Route path='/login' element={ <Login/> }/>
+                        <Route path='/wallet' element={ <Wallet/> }/>
+                        <Route path='/settings' element={ <Settings/> }/>
+                    </Routes>
+                </Fragment>
+            </Router>
+        </WalletContext.Provider>
     )
 }
 
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import SimpleLayout from '../../components/Layout/simple'
 import { Link } from 'react-router-dom'
+import { useWalletContextValue } from '../../hooks'
 
 const Login:React.FC = () => {
-    return(
+    const { jsonWallet } = useWalletContextValue()
+
+    return( 
         <SimpleLayout>
             <div className="container h-[475px]">
-                <div> This is a login screen!</div>
+                {
+                    jsonWallet === ' ' 
+                        ? <div>Oy mate this is your first time in Xfolio, welcome!</div>
+                        : <div>It is nice to see you again friend!</div>
+                }
                 <Link to='/wallet'>Sign me in!</Link>
             </div>
         </SimpleLayout>

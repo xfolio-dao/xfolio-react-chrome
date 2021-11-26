74b93a36c5802e67a7fc1abe0db16b435932dbb8
exakoss <danielkossman@yahoo.com>
2021-11-26 18:19:04 -0500

    Feat: Text3Xl

--- src/pages/login/mnemonic.tsx
+++ src/pages/login/mnemonic.tsx
@@ -5,6 +5,7 @@ import { useWalletContextValue } from '../../hooks'
 import { createConnectedRandomWallet } from '../../utils/ethers'
 import MnemonicPhraseView from '../../components/MnemonicPhraseView'
 import Button from '../../components/Button'
+import { Text3Xl } from '../../components/Text'
 
 const CreateWalletForm:React.FC = () => {
     const [mnemonic, setMnemonic] = useState('')
@@ -19,8 +20,9 @@ const CreateWalletForm:React.FC = () => {
 
     return(
         <div className='flex flex-col justify-center items-center'>
-            <p>Your mnemonic is:</p>
+            <Text3Xl text='Your new mnemonic phrase is:'/>
             <MnemonicPhraseView phrase={mnemonic}/>
+            <Text3Xl text='Make sure to write it down, because this phrase will be used for the recovery of your accounts in case you forget the password'/>
             <Button text='Set up your password' handleClick={() => navigate('/setPassword')}/>
         </div> 
     )

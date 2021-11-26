883ece43f6c6b10a464005c09d3df79608e4647e
exakoss <danielkossman@yahoo.com>
2021-11-26 17:28:27 -0500

    fix: wallet is now not persistent

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,7 @@
 import { useContext, useState, useMemo } from 'react'
 import { WalletContext } from '../context/walletContext'
 import { Wallet } from 'ethers'
-import { returnInitialLocalState, returnInitialSessionState } from '../utils/storage'
+import { returnInitialLocalState } from '../utils/storage'
 
 export const useLocalStorage = (storageKey:string) => {
     const [storedValue, setStoredValue] = useState(returnInitialLocalState(storageKey))
@@ -25,31 +25,9 @@ export const useLocalStorage = (storageKey:string) => {
     return [storedValue, setValue]
 }
 
-export const useSessionStorage = (storageKey:string) => {
-    const [storedValue, setStoredValue] = useState(returnInitialSessionState(storageKey))
-    const setValue = (value:any) => {
-        try {
-            const valueToStore = value instanceof Function ? value(storedValue) : value
-            // Save to local storage
-            if (typeof valueToStore === 'string') {
-                window.sessionStorage.setItem(storageKey, valueToStore)
-            }
-            else {
-                window.sessionStorage.setItem(storageKey, JSON.stringify(valueToStore))
-            }
-            // Save state
-            setStoredValue(valueToStore)
-        } catch (error) {
-            // A more advanced implementation would handle the error case
-            console.log(error)
-        }
-    }
-    return [storedValue, setValue]
-}
-
 export const useDefineWalletContext = () => {
     const [jsonWallet, setJsonWallet] = useLocalStorage('jsonWallet')
-    const [wallet, setWallet] = useSessionStorage('wallet')
+    const [wallet, setWallet] = useState<undefined | Wallet>(undefined)
 
     return useMemo(() => ({
         jsonWallet,
--- src/utils/storage/index.tsx
+++ src/utils/storage/index.tsx
@@ -16,21 +16,3 @@ export const returnInitialLocalState = (storageKey:string) => {
         return null
     }
 }
-
-export const returnInitialSessionState = (storageKey:string) => {
-    try {
-        const item = window.sessionStorage.getItem(storageKey)
-        if (item) {
-            if ((typeof item) === 'string') {
-                return item
-            } else {
-                return JSON.parse(item)
-            }
-        } else {
-            return null
-        }
-    } catch (error) {
-        console.log(error)
-        return null
-    }
-}

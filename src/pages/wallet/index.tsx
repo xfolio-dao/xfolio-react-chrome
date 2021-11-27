5c932e12282699a78cfda329d6342d92248f3c19
exakoss <danielkossman@yahoo.com>
2021-11-27 15:34:37 -0500

    fix: arb and opt balances in wallet

--- src/pages/wallet/index.tsx
+++ src/pages/wallet/index.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import DefaultLayout from '../../components/Layout'
 import { useWalletContextValue } from '../../hooks'
 import { useEthBalance } from '../../hooks/useEth'
+import { connectWalletToNetwork } from '../../utils/ethers'
 
 const Wallet:React.FC = () => {
     const { wallet } = useWalletContextValue()
     const balance = useEthBalance(wallet)
+    //fix later with proper API calls on multiple chains
+    const arbitrumBalance = useEthBalance(connectWalletToNetwork(wallet,'arbitrum'))
+    const optimismBalance = useEthBalance(connectWalletToNetwork(wallet,'optimism'))
 
     return(
         <DefaultLayout>
@@ -14,6 +18,21 @@ const Wallet:React.FC = () => {
                 <p>{ wallet.address }</p>
                 <p>Your mainnet balance:</p>
                 <p>{ balance }</p>
+                {
+                    arbitrumBalance !== '0.000' && arbitrumBalance !== '0'
+                        ? <div> <p>Your arbitrum balance:</p>
+                            <p>{ arbitrumBalance}</p>
+                        </div>
+                        : null
+                }
+                {
+                    optimismBalance !== '0.000' && optimismBalance !== '0'
+                        ? <div> <p>Your optimism balance:</p>
+                            <p>{ optimismBalance}</p>
+                        </div>
+                        : null
+                }
+                
             </div>
         </DefaultLayout>
     )

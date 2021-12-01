732e25e91391518ad577747b6aab7079266d7058
exakoss <danielkossman@yahoo.com>
2021-11-30 21:45:37 -0500

    feat: kovan and fuji testnets

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,3 +1,5 @@
 export const MAINNET_API_KEY = process.env.REACT_APP_ALCHEMY_MAINNET_KEY as string
 export const ARBITRUM_API_KEY = process.env.REACT_APP_ALCHEMY_ARBITRUM_KEY as string
 export const OPTIMISM_API_KEY = process.env.REACT_APP_ALCHEMY_OPTIMISM_KEY as string
+export const KOVAN_API_KEY = process.env.REACT_APP_ALCHEMY_KOVAN_KEY as string
+export const FUJI_API_KEY = process.env.REACT_APP_FUJI_KEY as string
